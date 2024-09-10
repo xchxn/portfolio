ab9a4a8cc67f332c235ffcb74fd131165b3b7ef3
xchxn <seokregi@gmail.com>
2024-09-10 15:47:37 +0900

    Favicon change and add route for nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostingsComponent } from './postings/postings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent},
+  { path: 'project', component: ProjectComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'postings', component: PostingsComponent },
+];
---
+++ src/app/postings/postings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postings',
+  standalone: true,
+  imports: [],
+  templateUrl: './postings.component.html',
+  styleUrl: './postings.component.scss'
+})
+export class PostingsComponent {
+
+}
